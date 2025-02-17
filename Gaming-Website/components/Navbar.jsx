bc4302b86f6c25bf33adb9b54003a10166625599
Daniel-Clerkson <danielclerkson68@gmail.com>
2025-02-17 13:17:13 +0100

    INstalled TailwindCss

---
+++ Gaming-Website/components/Navbar.jsx
@@ -0,0 +1,18 @@
+const Navbar = () => {
+    return (
+        <div>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
